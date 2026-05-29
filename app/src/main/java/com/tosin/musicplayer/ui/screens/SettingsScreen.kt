c47a3c71c55efc158daaec7cb143629c491a4044
Tosin-JD <oluwatosinjosephdurodola@gmail.com>
2026-05-29 22:13:07 +0100

    fix padding in settings

--- app/src/main/java/com/tosin/musicplayer/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/tosin/musicplayer/ui/screens/SettingsScreen.kt
@@ -33,9 +33,12 @@ fun SettingsScreen(
     var showResetDialog by remember { mutableStateOf(false) }
 
     Scaffold(
+//        contentWindowInsets = WindowInsets(0, 0, 0, 0),
         topBar = {
             TopAppBar(
                 title = { Text("Settings", fontWeight = FontWeight.Bold) },
+                windowInsets = WindowInsets(0, 0, 0, 0),
+
                 navigationIcon = {
                     IconButton(onClick = onNavigateBack) {
                         Icon(Icons.AutoMirrored.Rounded.ArrowBack, contentDescription = "Back")
